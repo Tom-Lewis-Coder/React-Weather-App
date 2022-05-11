ed55d117a39de797ebff704b4d8e3321dcc1742e
Tom-Lewis-Coder <tomlewis139@gmail.com>
2022-05-11 16:31:47 +0100

    'first commit'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+import DateBuilder from './Components/DateBuilder'
+import SearchBar from './Components/SearchBar'
+import Location from './Components/Location'
+import WeatherDisplay from './Components/WeatherDisplay'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  
+  const [query, setQuery] = useState("")
+  const [weather, setWeather] = useState({})
+
+  const api = {
+    key: 'c5a3da79e6f253015e9c856837f98a0c',
+    base: 'http://api.openweathermap.org/data/2.5/'
+  }
+
+const search = (evt) => {
+    if (evt.key === "Enter") {
+        fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+          .then(resp => resp.json())
+          .then(weatherData => { 
+          setWeather(weatherData)
+          setQuery("")
+        })
+    }
 }
 
-export default App;
+const handleChange = e => {
+  setQuery(e.target.value)
+}
+
+  const getIcon = iconCode => {
+    return `http://openweathermap.org/img/wn/${iconCode}@2x.png`
+  }
+
+  const imgDisplay = weather.main && Number(weather.weather[0].icon.replace(/[^\d]/g, ''))
+
+  return (
+    <main 
+    className={ weather.main && imgDisplay > 7 ? "weather-bad" : imgDisplay < 3 ? "app" : "weather-cloud" } 
+      >
+          <SearchBar 
+            loadWeather={search} 
+            handleChange={handleChange}
+            value={query} />
+        {( weather.main && 
+        <div>
+            <Location 
+              city={weather.name} 
+              country={weather.sys.country} />
+            <DateBuilder />
+            <WeatherDisplay 
+              temp={Math.round(weather.main.temp)} 
+              weather={weather.weather[0].main} 
+              src={getIcon(weather.weather[0].icon)} />
+        </div> )}
+    </main>
+  )
+}
+
+export default App
